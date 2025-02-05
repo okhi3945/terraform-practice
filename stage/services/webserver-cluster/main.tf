b1b0f21c63f8b5a12cebb9989ad80cd9629571cb
okhi3945 <yhoka@naver.com>
2025-02-05 11:21:17 +0900

    terraform module practice localization, outputs

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -4,6 +4,14 @@ provider "aws" {
 module "webserver_cluster" {
  source = "../../../modules/services/webserver-cluster"
  cluster_name = "webservers-stage"
- db_remote_state_bucket = "(YOUR_BUCKET_NAME)"
+ db_remote_state_bucket = "terraform-state-dudgkr3-wave"
  db_remote_state_key = "stage/data-stores/mysql/terraform.tfstate"
+ instance_type = "t3.micro"
+ min_size = 2
+ max_size = 2
+}
+
+output "alb_dns_name" {
+ value = module.webserver_cluster.alb_dns_name
+ description = "The domain name of the load balancer"
 }
