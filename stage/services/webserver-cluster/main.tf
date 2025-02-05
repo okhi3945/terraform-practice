0f629dd48c8b9f353f606b5863f31e32e3b09fe5
okhi3945 <yhoka@naver.com>
2025-02-05 11:25:02 +0900

    Initial commit of modules repo

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 module "webserver_cluster" {
- source = "../../../modules/services/webserver-cluster"
+ source = "github.com/okhi3945/modules//services/webserver-cluster?ref=v0.0.1"
  cluster_name = "webservers-stage"
  db_remote_state_bucket = "terraform-state-dudgkr3-wave"
  db_remote_state_key = "stage/data-stores/mysql/terraform.tfstate"
